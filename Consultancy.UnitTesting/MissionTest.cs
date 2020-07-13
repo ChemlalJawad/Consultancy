c744254a789a43c61123e4093cb9fb0c8eb6176c
ChemlalJawad <jawadchemlal@hotmail.com>
2020-07-13 14:59:32 +0200

    Add testing method and new way to test

--- Consultancy.UnitTesting/MissionTest.cs
+++ Consultancy.UnitTesting/MissionTest.cs
@@ -9,136 +9,229 @@ using Consultancy.Core.Exceptions;
 using Consultancy.Core.Const;
 using NUnit.Framework;
 using Consultancy.Core.Domain;
+using AutoFixture;
+using Consultancy.Core.Enum;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Consultancy.UnitTesting
 {
-    public class MissionTest : ServiceContext
+    public class MissionTest : TestingContext<MissionService>
     {
-        public MissionTest() : base(
-            new DbContextOptionsBuilder<ConsultingContext>()
-                .UseInMemoryDatabase("Test")
-                .Options)
-        { 
+        public MissionTest()
+        {
+            base.Setup();
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
         }
 
+        private ConsultingContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<ConsultingContext>()
+              .UseInMemoryDatabase(databaseName: "ConsultancyTest")
+              .Options;
+
+            var context = new ConsultingContext(options);
+            context.Database.EnsureDeleted();
+            SeedDatabase(context);
+            context.SaveChanges();
+            return context;
+        }
+
+        private void SeedDatabase(ConsultingContext context)
+        {
+            context.Consultants.AddRange(new List<Consultant> 
+            {
+                new Consultant
+                {
+                    Id = 21,
+                    Firstname = "Jawad",
+                    Lastname = "Chemlal",
+                    Experience = Experience.Medior
+                },
+                new Consultant
+                {
+                    Id = 22,
+                    Firstname = "Fabrice",
+                    Lastname = "Eboué",
+                    Experience = Experience.Senior
+                }
+            });
+            context.Missions.AddRange(new List<Mission>
+            {    
+                new Mission
+                {
+                    Id = 31,
+                    Name = "Genesis",
+                    MaximumRate = 600.00,
+                    ExperienceRequired = Experience.Senior
+                },
+                new Mission
+                {
+                    Id = 32,
+                    Name = "Microsoft",
+                    MaximumRate = 500.00,
+                    ExperienceRequired = Experience.Medior
+                },                
+                new Mission
+                {
+                    Id = 33,
+                    Name = "Google",
+                    MaximumRate = 400.00,
+                    ExperienceRequired = Experience.Medior
+                }
+            });
+            context.ConsultantMissions.AddRange(new List<ConsultantMission>
+            { 
+                new ConsultantMission
+                {
+                    Id =17,
+                    ConsultantId = 21,
+                    MissionId = 32,
+                    Rate = 400.00,
+                    IsActive = true,
+                    JobName = "Medior Dev"
+                },
+                 new ConsultantMission
+                {
+                    Id =18,
+                    ConsultantId = 22,
+                    MissionId = 31,
+                    Rate = 600.00,
+                    IsActive = true,
+                    JobName = "Senior Dev"
+                }, new ConsultantMission
+                {
+                    Id =19,
+                    ConsultantId = 22,
+                    MissionId = 32,
+                    Rate = 500.00,
+                    IsActive = false,
+                    JobName = "Medior Dev"
+                }
+            });
+           
+        }
         [Fact]
         public void AddConsultant_JobNameNotValidExcpetion()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);
-               // context.ConsultantMissions.Add(new ConsultantMission { Id = 3, ConsultantId = 1, MissionId = 1, JobName = "Jaja", Rate = 400.00, isActive = true });
-                //context.SaveChanges();
-                var addConsultant = new AddConsultantRequest
-                {
-                    ConsultantId = 1,
-                    MissionId = 1,
-                    Rate = 400.00
-                };
-                Action action = () => service.AddConsultant(addConsultant);
-                action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.JobnameNotExist);
-            }
+            var addConsultant = new AddConsultantRequest();
+                
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.JobnameNotExist);
+            
         }
-        
+      
         [Fact]
         public void AddConsultant_RateNotValidExcpetion()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);
-                //context.ConsultantMissions.Add(new ConsultantMission { Id = 2, ConsultantId = 3, MissionId = 3, JobName = "Jaja", Rate = 400.00, isActive = true });
-                //context.SaveChanges();
-                var addConsultant = new AddConsultantRequest
+            var addConsultant = new AddConsultantRequest
                 {
-                    ConsultantId = 3,
-                    MissionId = 3,
-                    JobName = "Java Dev"
+                    ConsultantId = 1,
+                    MissionId = 1,
+                    JobName = "Dev Lead"
                 };
-                Action action = () => service.AddConsultant(addConsultant);
-                action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.RateIsNotCompleted);
-            }
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.RateIsNotCompleted);           
         }  
         
         [Fact]
         public void AddConsultant_MissionNotValidExcpetion()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);
-                //context.ConsultantMissions.Add(new ConsultantMission { Id = 1, ConsultantId = 1, MissionId = 1, JobName = "Jaja", Rate = 400.00, isActive = true });
-                //context.SaveChanges();
-                
-                var addConsultant = new AddConsultantRequest
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
                 {
-                    ConsultantId = 1,
+                    ConsultantId = 21,
                     MissionId = 20,
                     JobName = "Java Dev",
                     Rate = 400.00
                 };
-
-                Action action = () => service.AddConsultant(addConsultant);
-                action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.MissioNotExist);
-            }           
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.MissioNotExist);          
+        }        
+        
+        [Fact]
+        public void AddConsultant_RateHighNotValidExcpetion()
+        {
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
+                {
+                    ConsultantId = 22,
+                    MissionId = 33,
+                    JobName = "Java Dev",
+                    Rate = 300.00
+                };
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.RateMaximumRequired);           
         }        
 
         [Fact]
         public void AddConsultant_ConsultantNotValidExcpetion()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);               
-                var addConsultant = new AddConsultantRequest
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
                 {
                     ConsultantId = 40,
-                    MissionId = 1,
+                    MissionId = 31,
                     JobName = "Java Dev",
                     Rate = 400.00
                 };
-
-                Action action = () => service.AddConsultant(addConsultant);
-                action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.ConsultantNotExist);
-            }           
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.ConsultantNotExist);           
         }
 
         [Fact]
         public void AddConsultant_ExperienceNotValidExcpetion()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);               
-                var addConsultant = new AddConsultantRequest
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
                 {
-                    ConsultantId = 1,
-                    MissionId = 1,
+                    ConsultantId = 21,
+                    MissionId = 31,
                     JobName = "Java Dev",
                     Rate = 400.00
                 };
-
-                Action action = () => service.AddConsultant(addConsultant);
-                action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.ExperienceMinimumRequired);
-            }           
+            Action action = () => ClassUnderTest.AddConsultant(addConsultant);
+            action.Should().ThrowExactly<NotValidException>().WithMessage(ErrorVariable.ExperienceMinimumRequired);            
         }
 
         [Fact]
+        public void AddConsultant_LastMissionNeedToBeFalse()
+        {
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
+            {
+                ConsultantId = 21,
+                MissionId = 33,
+                JobName = "Dev Lead",
+                Rate = 400.00
+            };
+            
+            var result = ClassUnderTest.AddConsultant(addConsultant);
+            result.Consultant.ConsultantMissions.Where(x => x.IsActive).Count().Should().Be(1);
+        }    
+        [Fact]
         public void AddConsultant_IsValid()
         {
-            using (var context = new ConsultingContext(ContextOptions))
-            {
-                var service = new MissionService(context);               
-                var addConsultant = new AddConsultantRequest
+            var context = InitializeContext();
+            InjectClassFor(context);
+            var addConsultant = new AddConsultantRequest
                 {
-                    ConsultantId = 3,
-                    MissionId = 3,
+                    ConsultantId = 22,
+                    MissionId = 33,
                     JobName = "Dev Lead",
-                    Rate = 500.00
+                    Rate = 400.00
                 };
-
-                var result = service.AddConsultant(addConsultant);
-                result.ConsultantId.Should().Be(3);
-                result.MissionId.Should().Be(3);
-                result.IsActive.Should().Be(true);
-                result.Rate.Should().Be(500.00);
-            }           
+    
+            var result = ClassUnderTest.AddConsultant(addConsultant);
+            result.ConsultantId.Should().Be(22);
+            result.MissionId.Should().Be(33);
+            result.IsActive.Should().Be(true);
+            result.Rate.Should().Be(400.00);     
         }
     }
 }
